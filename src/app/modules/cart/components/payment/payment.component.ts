c9728c1dbb52f9e87eea332f6b8dcaff0b5756bc
salman <najarsalman4@gmail.com>
2022-07-19 09:27:01 +0530

    default changes

--- src/app/modules/cart/components/payment/payment.component.ts
+++ src/app/modules/cart/components/payment/payment.component.ts
@@ -7,51 +7,6 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  buttonColor = "black";
-  buttonType = "buy";
-  isCustomSize = false;
-  buttonWidth = 240;
-  buttonHeight = 40;
-  isTop = window === window.top;
-
-  paymentRequest = {
-    apiVersion: 2,
-    apiVersionMinor: 0,
-    allowedPaymentMethods: [
-      {
-        type: "CARD",
-        parameters: {
-          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-          allowedCardNetworks: ["AMEX", "VISA", "MASTERCARD"]
-        },
-        tokenizationSpecification: {
-          type: "PAYMENT_GATEWAY",
-          parameters: {
-            gateway: "example",
-            gatewayMerchantId: "exampleGatewayMerchantId"
-          }
-        }
-      }
-    ],
-    merchantInfo: {
-      merchantId: "12345678901234567890",
-      merchantName: "Demo Merchant"
-    },
-    transactionInfo: {
-      totalPriceStatus: "FINAL",
-      totalPriceLabel: "Total",
-      totalPrice: "100.00",
-      currencyCode: "USD",
-      countryCode: "US"
-    }
-  };
-
-  onLoadPaymentData(event: { detail: any; }) {
-    console.log("load payment data", event.detail);
-  }
-
-
-
 
   isCOD: boolean = true;
   isPayOnline: boolean = false;
@@ -89,31 +44,32 @@ export class PaymentComponent implements OnInit {
       }
     }
 
-    // const paymentRequest: any = {
-    //   orderId: "12345",
-    //   customerId: "100",
-    //   transactionAmount: 500
-    // }
-    // var formData = new FormData();
-    // formData.append("paymentRequest", paymentRequest);
-    // this.orderService.pay(paymentRequest)
-    //   .subscribe(res => {
-    //     //this.redirectToPaytm(res);
-    //     console.log(res);
-    //   })
+    const paymentRequest: any = {
+      orderId: "12345",
+      customerId: "100",
+      transactionAmount: 500
+    }
+    var formData = new FormData();
+    formData.append("paymentRequest", paymentRequest);
+    this.orderService.pay(paymentRequest)
+      .subscribe(res => {
+        this.redirectToPaytm(res);
+        console.log(res);
+      })
   }
 
   redirectToPaytm(parameters: any) {
-    /*  var formData = new FormData();
+      var formData = new FormData();
      Object.keys(parameters).map(index => {
        formData.append(index, parameters[index]);
      });
      console.log(parameters);
+     debugger;
      console.log(formData.getAll('ORDER_ID'));
      this.orderService.getPaytmResponse(formData)
          .subscribe(res => {
            console.log(res);
-     }); */
+     });
     this.createPaymentForm(parameters);
   }
 
